e56de0d7e58820419603402a636bc332b5cd2f41
Grant Ramil <grantramil@yahoo.com>
2014-04-17 17:41:09 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import Flask, render_template, send_from_directory
+app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 
+        'ico/favicon.ico')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
